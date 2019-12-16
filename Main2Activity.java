776e42ae9c83384dff3ce4d5bddd84192d41d68b
Luis Sanz Goizueta <ataraxa@hotmail.com>
2019-12-16 19:07:35 +0100

    Add files via upload

--- Main2Activity.java
+++ Main2Activity.java
@@ -1,13 +1,38 @@
-package com.example.luis.intentsactivities;
+package com.example.prubaactivity;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
-
+TextView tv_mostrar_nombre;
+Button btn;
+EditText et_nombre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Intent i=getIntent();
+        Bundle b=i.getExtras();
+        et_nombre=findViewById(R.id.et_main2);
+        btn=findViewById(R.id.btn_volver);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+                i.putExtra("nombre_vuelta",et_nombre.getText().toString());
+                setResult(Activity.RESULT_OK,i);
+                finish();
+            }
+        });
+        tv_mostrar_nombre=findViewById(R.id.tv_mostrar_nombre);
+        String nombre=b.getString("nombre");
+        tv_mostrar_nombre.setText("Hola, te llamas "+nombre);
     }
 }
--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,44 @@
-package com.example.luis.intentsactivities;
+package com.example.prubaactivity;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_abrirActivity;
+    EditText et_nombre;
+    Context contexto;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        String nombre_vuelta=data.getExtras().getString("nombre_vuelta");
+        et_nombre.setText(nombre_vuelta);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        contexto=this;
+        //Inicializo el botón
+        btn_abrirActivity=findViewById(R.id.btn_enviar);
+        et_nombre=findViewById(R.id.et_nombre);
+        btn_abrirActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String dato=et_nombre.getText().toString();
+                Intent i=new Intent(contexto, Main2Activity.class);
+                i.putExtra("nombre", dato);
+                //startActivity(i);
+                startActivityForResult(i, 1);
+            }
+        });
     }
 }
